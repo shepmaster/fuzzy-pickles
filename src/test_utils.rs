b8f159ef6f84342eeb707c73196925191aeda544
Jake Goulding <jake.goulding@gmail.com>
2017-09-17 11:39:53 -0400

    Move `unwrap_as` macro to shared location

--- src/test_utils.rs
+++ src/test_utils.rs
@@ -1,5 +1,14 @@
 use super::*;
 
+macro_rules! unwrap_as {
+    ($e:expr, $p:path) => {
+        match $e {
+            $p(s) => s,
+            _ => panic!("Not a {}: {:?}", stringify!($p), $e),
+        }
+    }
+}
+
 macro_rules! assert_extent {
     ($parsed:expr, $extent:expr) => (assert_eq!($parsed.extent(), $extent));
 }
