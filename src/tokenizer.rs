5d1d6d64199bef3eb7804c5881e06ebbd3ec3baa
Jake Goulding <jake.goulding@gmail.com>
2020-05-18 08:02:05 -0400

    Use single character patterns

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -695,15 +695,15 @@ fn whitespace<'s>(_pm: &mut Master<'s>, pt: Point<'s>) -> Progress<'s, Extent> {
 fn comment_or_doc_comment<'s>(_pm: &mut Master<'s>, pt: Point<'s>) -> Progress<'s, Token> {
     let spt = pt;
     if pt.s.starts_with("///") && !pt.s.starts_with("////") {
-        let eol = pt.s.find("\n").unwrap_or(pt.s.len());
+        let eol = pt.s.find('\n').unwrap_or(pt.s.len());
         let (pt, _) = try_parse!(spt.consume_to(Some(eol)).map_err(|_| Error::ExpectedComment));
         Progress::success(pt, Token::DocCommentOuterLine(ex(spt, pt)))
     } else if pt.s.starts_with("//!") {
-        let eol = pt.s.find("\n").unwrap_or(pt.s.len());
+        let eol = pt.s.find('\n').unwrap_or(pt.s.len());
         let (pt, _) = try_parse!(spt.consume_to(Some(eol)).map_err(|_| Error::ExpectedComment));
         Progress::success(pt, Token::DocCommentInnerLine(ex(spt, pt)))
     } else if pt.s.starts_with("//") {
-        let eol = pt.s.find("\n").unwrap_or(pt.s.len());
+        let eol = pt.s.find('\n').unwrap_or(pt.s.len());
         let (pt, _) = try_parse!(spt.consume_to(Some(eol)).map_err(|_| Error::ExpectedComment));
         Progress::success(pt, Token::CommentLine(ex(spt, pt)))
     } else if pt.s.starts_with("/**") && !pt.s.starts_with("/***") && !pt.s.starts_with("/**/") {
